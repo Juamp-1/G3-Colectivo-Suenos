cf525fcfabb0b550a96b2e9018b21eabf5aaba48
rodrigocc28 <rodrigocc.28@hotmail.com>
2024-10-24 10:36:25 -0300

    Login Nuevo

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/recuperar', (req,res) => res.sendFile(path.join(__dirname, 'views', 'recuperar.html')));
 
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));

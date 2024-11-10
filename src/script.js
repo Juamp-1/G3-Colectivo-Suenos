97ab1411575b17cee052133b8a81c34ed8752d14
Juamp-1 <juanpgodoy04@gmail.com>
2024-11-10 00:30:13 -0300

    footer icon changed (x & tiktok eliminated) whatsapp added

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('textarea').forEach(textarea => {
+    textarea.addEventListener('input', function() {
+        this.style.height = 'auto'; // Resetea la altura
+        this.style.height = (this.scrollHeight) + 'px'; // Ajusta la altura según el contenido
+    });
+});
